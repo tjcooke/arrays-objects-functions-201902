d912fda2996f9cddfc82b112e6eab02430d677b8
Tyler Cooke <tylerstouchpad@gmail.com>
2019-03-06 09:07:11 -0500

    fixed commenting on several lines

--- scripts/index.js
+++ scripts/index.js
@@ -58,12 +58,13 @@ console.log(squareNumbers(arr));
 
 // Cities 1 - Write a function which takes an array of city objects like such:
 
-// var cities = [
-//     { name: 'Los Angeles', temperature: 60.0},
-//     { name: 'Atlanta', temperature: 52.0 },
-//     { name: 'Detroit', temperature: 48.0 },
-//     { name: 'New York', temperature: 80.0 }
-//   ];
+var cities = [
+    { name: 'Los Angeles', temperature: 60.0},
+    { name: 'Atlanta', temperature: 52.0 },
+    { name: 'Detroit', temperature: 48.0 },
+    { name: 'New York', temperature: 80.0 }
+];
+
 //   as input and returns a new array containing the cities whose temperature is cooler than 70 degrees.
 
 
